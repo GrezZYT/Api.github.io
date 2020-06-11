4fbb5f3513cb307c11aa0b96233215710a351470
Gerardo Pérez <ISnape.jilc45gmail.com>
2020-06-11 17:02:34 -0500

    Callbacks

---
+++ CALLBACKS/app.js
@@ -0,0 +1,45 @@
+// Listado de paises
+const paises = ['Francia', 'España', 'México', 'Portugal', 'Inglaterra'];
+
+// Se agrega un nuevo pais despues de 2 segundos
+function nuevoPais(pais, callback){
+    setTimeout(function(){
+        paises.push(pais);
+        callback();
+    }, 2000);
+}
+
+function mostrarPaises(){
+    setTimeout(function(){
+        let html = '';
+        paises.forEach(function(pais){
+            html += `<li>${pais}</li>`;
+        });
+        document.getElementById('app').innerHTML = html;
+    }, 1000);
+}
+
+// Agregar nuevo pais
+nuevoPais('Alemania', mostrarPaises);
+
+// Mostrar paises
+mostrarPaises();
+
+
+
+/*
+// Callback en Foreach
+
+const ciudades = ['Londres', 'New York', 'Madrid', 'Paris'];
+
+// Inline callback
+ciudades.forEach(function(ciudad){
+    console.log(ciudad);
+})
+
+// Con función definida
+function callback(pais){
+    console.log(ciudad);
+}
+ciudades.forEach(callback);
+*/
